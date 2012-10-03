efe45c648f60662484c3f5b8aae95aa6a738cbd3
Filip Procházka <hosiplan@gmail.com>
2012-10-03 15:57:46 +0200

    tests

--- tests/PPAPIServiceTest.php
+++ tests/PPAPIServiceTest.php
@@ -7,46 +7,45 @@
  */
 class PPAPIServiceTest extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @var PPAPIService
-     */
-    protected $object;
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
-    protected function setUp()
-    {
-        $this->object = new PPAPIService;
-    }
+	/**
+	 * @var PPAPIService
+	 */
+	protected $object;
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
-    }
 
-    /**
-     * @test
-     */
-    public function testSetServiceName()
-    {
-       $this->object->setServiceName('Invoice');
-       $this->assertEquals('Invoice',$this->object->serviceName);
-       $object = new PPAPIService('ServiceName');
-        $this->assertEquals('ServiceName',$object->serviceName);
-    }
 
-    /**
-     * @test
-     */
-    public function testMakeRequest()
-    {
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 */
+	protected function setUp()
+	{
+		$this->object = new PPAPIService;
+	}
 
-    }
+
+
+	/**
+	 *
+	 */
+	public function testSetServiceName()
+	{
+		$this->object->setServiceName('Invoice');
+		$this->assertEquals('Invoice', $this->object->serviceName);
+		$object = new PPAPIService('ServiceName');
+		$this->assertEquals('ServiceName', $object->serviceName);
+	}
+
+
+
+	/**
+	 *
+	 */
+	public function testMakeRequest()
+	{
+		$this->markTestIncomplete("Not implemented");
+	}
 }
 
 
